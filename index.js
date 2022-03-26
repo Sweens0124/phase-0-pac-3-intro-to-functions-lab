a7a5d5cb19b1bd8ef7f6caf49dd30955551468d1
Lucas Sweeney <lsweeney0124@gmail.com>
2022-03-26 09:00:22 -0600

    commit tree 6f8a6181f0b0ff4c29fc19eaaaa3982184f89994

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+console.log(shout("hello!"));
+console.log(whisper("Hello!"));
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+logShout("hello!");
+logWhisper("hello!");
+
+function sayHiToHeadphonedRoommate() {
+  if (string === toLowerCase(string));
+  {
+    console.log("I can't hear you!");
+  }
+  if (string === toUpperCase(string));
+  {
+    console.log("YES INDEED!");
+    //   } else (string === "Let's have dinner tonight!") {
+    //       return "I'd love to!"
+    //   }
+  }
+}
+sayHiToHeadphonedRoommate("hello!");
